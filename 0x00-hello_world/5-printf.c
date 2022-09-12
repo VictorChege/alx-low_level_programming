a3228009154fde3a220d0e7839ef509a253e1014
VictorChege <chegevictor4@gmail.com>
2022-09-13 00:59:31 +0300

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#!/bin/bash
+/**
+ * main-Entry point
+ *
+ * Description: 'Printing to the stdout using printf function'
+ *
+ * Return: Always 0 (Success)
+ * /
+#include <stdio.h>
+int main(void)
+{
+        printf("\"with proper grammar, but the outcomeis a piece of art,");
+        return (0);
+} 
