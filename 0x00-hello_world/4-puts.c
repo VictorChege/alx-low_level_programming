140501fe556381cace2ce543a0e177c28ab9241e
VictorChege <chegevictor4@gmail.com>
2022-09-12 23:45:41 +0300

    name

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main-Entrypoint
+ *
+ * Description: 'Printing to the stdout using puts function'
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
