70fd22eaed2831b7ca8b790cafe3160300f7eba2
Victor Chege <chegevictor4@gmail.com>
2022-09-20 23:22:24 +0300

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#include <stio.h>
+
+/**
+ * main entry point
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+	n = 402;
+	printf("n=%d\n", n);
+	reset_to_98(&n);
+	printf("n-%d\n",n);
+	return (0);
+}
