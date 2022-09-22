9f7c451a4f407e974285b06e836eb01850def4c7
Victor Chege <chegevictor4@gmail.com>
2022-09-23 00:03:00 +0300

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,11 +1,8 @@
 #include "main.h"
 
 /**
- * _strcat - concatenates two strings
- * @dest: string to append to
- * @src: string to add
- *
- * Return: a pointer to the resulting string
+ * main entry point
+ * Return: 0 (success)
  */
 char *_strcat(char *dest, char *src)
 {
