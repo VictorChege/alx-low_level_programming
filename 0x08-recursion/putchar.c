c4dad0c324ba56cb0910ea6806a1610f35cf7ff8
Victor Chege <chegevictor4@gmail.com>
2023-08-02 16:30:20 +0300

    putchar.c

--- 0x08-recursion/putchar.c
+++ 0x08-recursion/putchar.c
@@ -1,15 +1,13 @@
-#ifndef _putchar_H_
-#define _putchar_H_
+#include <unistd.h>
 
-int _putchar(char);
-void _puts_recursion(char *s);
-void _print_rev_recursion(char *s);
-int _strlen_recursion(char *s);
-int factorial(int n);
-int _pow_recursion(int x, int y);
-int _sqrt_recursion(int n);
-int is_prime_number(int n);
-int is_palindrome(char *s);
-int wildcmp(char *s1, char *s2);
-
-#endif
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
